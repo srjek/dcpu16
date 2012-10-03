71e4f6b84e66743a9b8e7972770c3046a63d8a47
Josh Kilmer <srjek2@gmail.com>
2012-10-03 18:00:13 -0500

    One step closer to pesudo assembly
    Signed-off-by: Josh Kilmer <srjek2@gmail.com>

--- compiler.py
+++ compiler.py
@@ -2,12 +2,127 @@ import sys
 
 def printError(error, lineNum, file=sys.stderr):
     print("Line " + str(lineNum) + ": " + str(error), file=file)
+
+class code0:
+    def __init__(self, token):
+        self.lineNum = token[0]
+    def needNextCode(self):
+        return False
+    def receiveNextCode(self, code):
+        return
+    def __repr__(self):
+        return "?"
+
+class statement(code0):
+    def __init__(self, token):
+        code0.__init__(self, token)
+        if token[1] != "statement":
+            printError("Passed token was a \"" + token[1] + "\" instead of a statement.", self.lineNum)
+        self.statement = token[2]
+    def __repr__(self):
+        return self.statement + ";"
+class declaration(code0):
+    def __init__(self, token):
+        code0.__init__(self, token)
+        if token[1] != "declaration":
+            printError("Passed token was a \"" + token[1] + "\" instead of a declaration.", self.lineNum)
+        self.type = token[2]
+        self.name = token[3]
+    def __repr__(self):
+        return self.type + " " + self.name + ";"
+class block(code0):
+    def __init__(self, token):
+        code0.__init__(self, token)
+        if token[1] != "{":
+            printError("Passed token was a \"" + token[1] + "\" instead of a '{'.", self.lineNum)
+        self.code = []
+        self.closed = False
+    def needNextCode(self):
+        return not self.closed
+    def receiveNextCode(self, code):
+        if len(self.code) > 0 and self.code[-1].needNextCode():
+            self.code[-1].receiveNextCode(code)
+        elif code == "}":
+            self.closed = True
+        else:
+            self.code.append(code)
+    def __repr__(self):
+        if len(self.code) == 0: return ";"
+        result = "{\n"
+        for c in self.code:
+            tmp = repr(c).split("\n");
+            for line in tmp:
+                result += "\t" + line + "\n"
+        return result + "}"
+
+class op0(code0):
+    def __init__(self, token):
+        code0.__init__(self, token)
+        self.code = None
+        if token[1] != "operation":
+            printError("Passed token was a \"" + token[1] + "\" instead of a operation.", self.lineNum)
+    def needNextCode(self):
+        if self.code == None:
+            return True
+        return self.code.needNextCode()
+    def receiveNextCode(self, code):
+        if self.code == None:
+            self.code = code
+        else:
+            self.code.receiveNextCode(code)
+class opWhile(op0):
+    def __init__(self, token):
+        op0.__init__(self, token)
+        if token[2].lower() != "while":
+            printError("Operation was \"" + token[2] + "\", not \"while\" as expected.", self.lineNum)
+        self.test = token[3]
+    def printError(self, error):
+        printError(error, self.lineNum)
+    def __repr__(self):
+        return "while (" + self.test + ")" + " " + repr(self.code)
+class opFor(op0):
+    def __init__(self, token):
+        op0.__init__(self, token)
+        if token[2].lower() != "for":
+            printError("Operation was \"" + token[2] + "\", not \"for\" as expected.", self.lineNum)
+        
+        tmp = token[3].split(";")
+        if len(tmp) < 3:
+            printError("Expected ';' before ')'", self.lineNum)
+            tmp.append("")
+            tmp.append("")
+        if len(tmp) > 3:
+            printError("Unexpected ';' before ')'", self.lineNum)
+            
+        self.initializer = tmp[0]
+        self.test = tmp[1]
+        self.increment = tmp[2]
+        
+    def printError(self, error):
+        printError(error, self.lineNum)
+    def __repr__(self):
+        return "for (" + self.initializer + ";" + self.test + ";" + self.increment + ")" + " " + repr(self.code)
+        
+
+def operation(token):
+    if token[1].lower() != "operation":
+        printError("Passed token was a \"" + token[1] + "\" instead of a operation.")
+    if token[2].lower() == "while":
+        return opWhile(token)
+    elif token[2].lower() == "for":
+        return opFor(token)
+    elif token[2].lower() == "if":
+        return opIf(token)
+    elif token[2].lower() == "do":
+        return opDoWhile(token)
+    else:
+        printError("Operation \"" + token[2] + "\" is unknown")
     
 class reader:
     symbols = ("+", "-", "*", "/", "%", "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=",
                "~", "&", "|", "^", "<<", ">>", "!", "&&", "||", "?", ":", "==", "!=", "(", ")", "++", "--",
                ".", "->", "<", "<=", ">", ">=", "[", "]", ",", '"', "'")
-    operations = ("while", "for", "if")
+    operations = ("while", "for", "if", "do")
     def __init__(self):
         self.curPart = ""
         self.mode = "none"
@@ -17,13 +132,21 @@ class reader:
         self.grouping_level = 0
         self.comment = None
         self.tokens = []
+        self.parseLevel = 0
+        self.tokens2 = []
+        self.code = []
     def printError(self, error, lineNum=None, charNum=None):
         if lineNum == None:
             lineNum = self.lineNum
         if charNum != None:
             error = "Char " + str(charNum) + ": " + error
         printError(error, lineNum)
-
+    #This compiler prints code as it is being parsed/translated
+    #This function controls what actually gets printed
+    def printCode(self, code, compilerLevel):
+        #if compilerLevel > 2:  #First 2 code levels are just echos of the input code
+            print(code)
+    
     def getParam(self, startI, endChars):
         printError = self.printError
         i = startI
@@ -41,11 +164,47 @@ class reader:
             param += " " + token
             i += 1
         return (param[1:], i)
+    def parseTokens2(self):
+        printError = self.printError
+        printCode = self.printCode
+        while len(self.tokens2) > 0:
+            nextToken = self.tokens2[0]
+            self.tokens2 = self.tokens2[1:]
+            tmp = None
+            if nextToken[1] == "{":
+                tmp = block(nextToken)
+            elif nextToken[1] == "}":
+                tmp = "}"
+            elif nextToken[1] == "nothing":
+                tmp = statement((nextToken[0], "statement", ""));
+                #printCode(";", 2)
+            elif nextToken[1] == "operation":
+                tmp = operation(nextToken)
+                #printCode(nextToken[2] + " (" + nextToken[3] + ")", 2)
+            elif nextToken[1] == "declaration":
+                tmp = declaration(nextToken)
+                #printCode(nextToken[2] + " " + nextToken[3] + ";", 2)
+            elif nextToken[1] == "statement":
+                tmp = statement(nextToken)
+                #printCode(nextToken[2] + ";", 2)
+            else:
+                print("Failed to parse \""+nextToken[1]+"\" token during level 2 parse")
+                continue
+            if len(self.code) > 0 and self.code[-1].needNextCode():
+                self.code[-1].receiveNextCode(tmp)
+                if not self.code[-1].needNextCode():
+                    printCode(repr(self.code[-1]), 2);
+                continue
+            self.code.append(tmp);
+            if not tmp.needNextCode():
+                printCode(repr(tmp), 2);
+            
     def parseTokens(self):
         printError = self.printError
+        printCode = self.printCode
         while len(self.tokens) > 0:
             nextToken = self.tokens[0]
-            if nextToken[2] in self.operations:
+            if nextToken[2].lower() in self.operations:
                 if self.tokens[1][2] != "(":
                     printError("Expected \"(\" after \"" + nextToken + "\" at char " + str(self.tokens[1][1]), self.tokens[1][0])
                 param = ""
@@ -53,15 +212,26 @@ class reader:
                 while i < len(self.tokens) and self.tokens[i][2] != ")":
                     param += " " + self.tokens[i][2]
                     i += 1
-                print(nextToken[2] + " (" + param[1:] + ")")
-            elif nextToken[2] in ("int",):
+                printCode(nextToken[2] + " (" + param[1:] + ")", 1)
+                self.tokens2.append((nextToken[0], "operation", nextToken[2], param[1:]))
+                self.tokens = self.tokens[i+1:]
+            elif nextToken[2] == ";":
+                printCode(";", 1)
+                self.tokens2.append((nextToken[0],"nothing"));
+                self.tokens = self.tokens[1:]
+            elif nextToken[2] in ("{", "}"):
+                printCode(nextToken[2], 1);
+                self.tokens2.append((nextToken[0], nextToken[2]))
+                self.tokens = self.tokens[1:]
+            elif nextToken[2].lower() in ("int",):
                 name = self.tokens[1]
                 if not (name[2].isalnum() and name[2][0].isalpha()):
                     printError("Char " + str(name[1]) + ": Variable names must be alphanumeric and start with a letter", name[0])
                     (param, i) = self.getParam(3, (";",))
                     self.tokens = self.tokens[i+1:]
                     return
-                print(nextToken[2]+" "+name[2]+";")
+                printCode(nextToken[2]+" "+name[2]+";", 1)
+                self.tokens2.append((nextToken[0], "declaration", nextToken[2], name[2]))
                 i = 2
                 while self.tokens[i][2] != ";":
                     if self.tokens[i][2] not in (";", "=", ","):
@@ -69,18 +239,29 @@ class reader:
                         (param, i) = self.getParam(3, (";",))
                         break
                     if self.tokens[i][2] == "=":
+                        tmpLineNum = self.tokens[i+1][0]
                         (param, i) = self.getParam(i+1, (";", ","))
-                        print(name[2]+" = "+param+";")
+                        printCode(name[2]+" = "+param+";", 1)
+                        self.tokens2.append((tmpLineNum, "statement", name[2]+" = "+param))
                     if self.tokens[i][2] == ",":
                         name = self.tokens[i+1];
                         if not (name[2].isalnum() and name[2][0].isalpha()):
                             printError("Char " + str(name[1]) + ": Variable names must be alphanumeric and start with a letter", name[0])
                             (param, i) = self.getParam(3, (";",))
                             break
+                        printCode(nextToken[2]+" "+name[2]+";", 1)
+                        self.tokens2.append((self.tokens[i+1][0], "declaration", nextToken[2], name[2]))
                         i += 2
-                        print(nextToken[2]+" "+name[2]+";")
                 self.tokens = self.tokens[i+1:]
-                    
+            else:
+                statement = nextToken[2]
+                i = 1
+                while (self.tokens[i][2] != ";"):
+                    statement += " " + self.tokens[i][2]
+                    i += 1
+                printCode(statement+";", 1)
+                self.tokens2.append((nextToken[0], "statement", statement))
+                self.tokens = self.tokens[i+1:]
         return
     
     def pushToken(self, charNum=None, token=None):
